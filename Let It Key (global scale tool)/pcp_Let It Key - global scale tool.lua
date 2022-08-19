e1195803b8483bf595260e2a0adac62337e2e6c3
oooOOOOOooo <5218005+s0u7h@users.noreply.github.com>
2022-08-19 21:29:29 +0800

    .

--- Let It Key (global scale tool)/pcp_Let It Key - global scale tool.lua
+++ Let It Key (global scale tool)/pcp_Let It Key - global scale tool.lua
@@ -5,7 +5,7 @@
  Forked from IX Snap To Key and Global Sliders, Copyright 2014 Philip S. Considine, and Easy Scale 1.4 (Snap to Key mod) by dissofiddle
  License: GPL - http://www.gnu.org/licenses/gpl.html
 @links Repository https://github.com/s0u7h/pcp/
-@version 1.36
+@version 1.37
 @metapackage
 @provides [nomain] .
   [main] pcp_Remove Let It Key from all non-instrument tracks.lua
